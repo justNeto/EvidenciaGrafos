71c34223948888431972878e8b558fe89659b62f
H3cth0r <hectorsucre13@gmail.com>
2021-11-23 08:35:22 -0600

    Documentation of grafo file

--- NoDirigidoAdyacencia/grafo.h
+++ NoDirigidoAdyacencia/grafo.h
@@ -33,11 +33,18 @@ class GraphAL{
     int number_vertices;
     Hashtable<N, LinkedList<N>> adj_list =  Hashtable<N, LinkedList<N>>();
     public:
+    /*
+    Undirected listed graph constructor.
+    Recives number of vertices to be added or an aproximate.
+    */
     GraphAL(int number_vertices){
         this->number_vertices = number_vertices;
         int initial_table_size = (int)pow(3, number_vertices); 
         adj_list = Hashtable<N, LinkedList<N>>(initial_table_size);              // creating hastable accoring to size
     }
+    /*
+    Default constructor, sets the number o vertices to cero.
+    */
     GraphAL(){
       this->number_vertices = 0;
     }
@@ -49,63 +56,92 @@ class GraphAL{
 
     /*
     add vertices to hashtable in a vertices.
-    set value as an empty LinkedList<N>
+    Recives a list with the vertices to be added and the size of array.
+    This makes it easy to add vertices.
     */
     bool set_vertices(N *keys, int size_array){
         LinkedList<N> empty_linked_list;
         for(int i = 0; i < size_array;i++) adj_list.put(keys[i], empty_linked_list);
         return true;
     }
+    /*
+    This method checks the existance of some vertex in the list.
+    Recives the key or value to be searched.
+    */
     bool check_vertex(N k){
         return adj_list.contains_key(k);
     }
+    /*
+    This method returns a row.
+    Key or value as parameter to get the selected row
+    */
     LinkedList<N> get_row(N k){
         return adj_list.get(k);
     }
+    /*
+    Checks if the list of adjacent vertex in some key is empty or not.
+    Recives key or value as parameter.
+    */
     bool empty_row(N k){
         if(adj_list.get(k).is_empty()) return true;
         return false;
     }
+    /*
+    This method prints the row, depending on inputen 
+    key or value as parameter.
+    */
     void print_row(N k){
         adj_list.get(k).print();
     }
+    /*
+    This bethod enables to create an Edge.
+    Recives the two value to be linked as parameters.
+    returns a boolean condition depending if there was a failure or not
+    */
     bool add_edge(N key_one, N key_two){
         if(adj_list.contains_key(key_one)== false) return false;
         if(adj_list.live_edge(key_one, key_two)==true) return true;
         adj_list.add_value_to_row(key_one, key_two);
         adj_list.add_value_to_row(key_two, key_one);
         return true;
-    }
+    }/*
+    This method adds a vertex to list.
+    Recives the value of the vertex to be added.
+    */
     bool add_vertex(N k){
         LinkedList<N> empty_linked_list;
         adj_list.put(k, empty_linked_list);
         return true;
     }
-
+    /*
+    Method for moving around the graph and getting in return
+    a linked list with all the value on the List, whithout repetitions.
+    Recives the stating vertex as parameter.
+    */
     LinkedList<N> bfs(N k){
         // N k == starting vertex
-
         LinkedList<N> queue = LinkedList<N>();  // queue to be returned
         LinkedList<N> vertex_list = LinkedList<N>();
         LinkedList<N> adjacent_list = LinkedList<N>();
-
-        queue.push(k);
-        
+        queue.push(k);        
         int current_vertex;
         while(!queue.is_empty()){
             current_vertex = queue.pop();
             if(!vertex_list.contains(current_vertex))vertex_list.push(current_vertex);
-
             adjacent_list = adj_list.get(current_vertex);
             for(int i = 0; i < adjacent_list.length(); i++){
                 if(!vertex_list.contains(adjacent_list.get(i))) queue.push(adjacent_list.get(i));
             }
         }
-
         return vertex_list;
-
     }
 
+    /*
+    The next two method work as one together.
+    This methods have the same functionality as the last one.
+    This is a recursive method.
+    Recives the starting vertex value.
+    */
     LinkedList<N> auxiliar_visited = LinkedList<N>();
     void auxiliar_dfs(N k){
         auxiliar_visited.push(k); // make current node as discovered or pushed.
@@ -119,7 +155,9 @@ class GraphAL{
         return auxiliar_visited;
 
     }
-
+    /*
+    Ostream operator overloading for printing the graph
+    */
     friend ostream& operator<<(ostream& os,const GraphAL<N> &dt){
         os << dt.adj_list;
         return os; 
--- NoDirigidoAdyacencia/main.cpp
+++ NoDirigidoAdyacencia/main.cpp
@@ -27,22 +27,8 @@ int main(){
     */
 
     GraphAL<char> grafo(5);
-    // int vertex[4] = {'A', 'B', 'C', 'D'};
-    // cout << "creating vertices: "<< grafo.set_vertices(vertex, sizeof(vertex)/sizeof(vertex[0])) << endl;
-    // cout << "live vertex: " << grafo.check_vertex('A')<< endl;
-    // cout << "key row list: " ; grafo.print_row('A'); cout<< "========" <<endl;
-    // cout << "Adding A-B edge: " <<grafo.add_edge('A', 'B') <<endl;
-    // cout << "Adding A-B edge(second try): " <<grafo.add_edge('A', 'B') <<endl;
-    // cout << "key row A list: "; grafo.print_row('A');
-    // cout << "key row B list: "; grafo.print_row('B');
-    // // cout << "adding vertex E: " << grafo.add_vertex('E') << endl;
-    // cout << "=================" << endl;
-    // grafo.add_edge('A', 'C');
-    // grafo.add_edge('B', 'C');
-    // grafo.add_edge('A', 'D');
-    // cout << grafo << endl;
-    // cout << grafo.bfs('A') << endl;
-    
+
+
     char vertex[5] = {'0', '1', '2', '3', '4'};
     cout << "creating vertices: "<< grafo.set_vertices(vertex, sizeof(vertex)/sizeof(vertex[0])) << endl;
     grafo.add_edge('0', '1');
