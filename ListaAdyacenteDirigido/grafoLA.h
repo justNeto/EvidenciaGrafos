21993f1a605493c9070eb3f842be6dd17c10a096
Eduardo Angeles <38306190+Edu4000@users.noreply.github.com>
2021-11-25 15:00:56 -0600

    Add files via upload

--- ListaAdyacenteDirigido/grafoLA.h
+++ ListaAdyacenteDirigido/grafoLA.h
@@ -2,98 +2,105 @@
 #include <iostream>
 #include <list>
 #include <limits>
+#include <algorithm>
+#include <queue>
 #include "tablahash.h"
 
 using namespace std;
 
-class GrafoMA{
+bool sortbysec(const pair<int,int> &a,
+			   const pair<int,int> &b)
+{
+	return (a.second < b.second);
+}
+
+template<typename D>
+class GrafoLA_Dirigido{
 	private:
-	int nVertices;
-	vector<int>* matriz;
-
+		int numVertice;
+		Hashtable<D,vector<pair<D,int>>*> tablaGrafos;
 	public:
-	// O(n^2)
-	GrafoMA(int nVertices){
-		this->nVertices = nVertices;
-		this->matriz = new vector<int>[nVertices];
-
-		for (int i = 0; i < nVertices; i++){
-			for (int j = 0; j < nVertices; j++){
-				matriz[i].push_back(0);
-			}
+		// Constructor
+		GrafoLA_Dirigido<D>(int numVertice){
+			this->numVertice = numVertice;
+			this->tablaGrafos = Hashtable<D,vector<pair<D,int>>*>(numVertice);
 		}
-	}
 
-	// O(1)
-	void add_edge(int s, int d){
-		matriz[s][d] = 1;
-		matriz[d][s] = 1;
-	}// O(1)
-	void add_edge_directed(int s, int d){
-		matriz[s][d] = 1;
-	}
+		// Adding vertex with lable 'vertex'
+		void add_vertex(D vertex){
+			this->tablaGrafos.put(vertex, new vector<pair<D,int>>());
+		}
 
-	// O(n^2)
-	void print(){
-		for (int i = 0; i < nVertices; i++){
-			for (int j = 0; j < nVertices; j++){
-				cout << matriz[i].at(j) << " ";
+		// Adding edge from vertex 'a' to 'b' with a weight
+		void add_edge(D a, D b, int weight){
+			vector<pair<D,int>>* edges = this->tablaGrafos.get(a);
+			pair<D,int> edge = pair<D,int>(b,weight);
+			edges->push_back(edge);
+		}
+
+		// Geting index where 'vertex' is located in 'edges'
+		int index_of_vertex(vector<pair<D, int>> edges, int vertex){
+			for(int i = 0; i < edges.size(); i++){
+				if(edges[i].first == vertex){
+					return i;
+				}
+			}
+			return 0;
+		}
+
+		// Dijksta's algorithm to find shortest path
+		void path_dijkstra(D start, D end){
+			int inf = numeric_limits<int>::max();
+			vector<D> vertices = tablaGrafos.keys();
+
+			// Vector of pair values ('vertex', weight between start and 'vertex')
+			vector<pair<D,int>> routes;
+
+			// First fill with vertices and associate inf as weight
+			for(D vertex : vertices){
+				routes.push_back(pair<D,int>(vertex,inf));
+			}
+			// Vertex at start has weight 0
+			routes.at(index_of_vertex(routes,start)).second = 0;
+
+			vector<pair<D,int>> destinies;
+			D current_vertex;
+			for(int i = 0; i < routes.size(); i++){
+				sort(routes.begin(), routes.end(), sortbysec);
+				current_vertex = routes.at(i).first;
+				// Destiny vertices from current vertex;
+				destinies = *tablaGrafos.get(current_vertex);
+
+				for(pair<D,int> data : destinies){
+					if(data.second < routes.at(index_of_vertex(routes,data.first)).second){
+						routes.at(index_of_vertex(routes,data.first)).second = data.second + routes.at(index_of_vertex(routes,current_vertex)).second;
+					}
+				}
+			}
+			cout << "FINAL RESULT" << endl;
+			for(auto results : routes){
+				cout << "v " << results.first << " -- cum_w: " << results.second << endl;
 			}
 			cout << endl;
+			return;
 		}
-	}
-};
 
-class GrafoLL_Dirigido{
-	private:
-	int nVertices;
-	Hashtable<int, vector<vector<int>>*>* listAD;
-	
-	public:
-	// O(n^2)
-	GrafoLL_Dirigido(int nVertices){
-		this->nVertices = nVertices;
-		this->listAD = new Hashtable<int, vector<vector<int>>*>(nVertices);
-	}
-
-	// O(1)
-	void add_vertex(int s){
-		listAD->put(s, new vector<vector<int>>());
-	}
-
-	// O(1)
-	void add_edge(int s, int d, int weight){
-		vector<vector<int>>* edges = listAD->get(s);
-		vector<int> res = {d , weight};
-		edges->push_back(res);
-	}
-	
-	// O(1)
-	void shortest_path(int end){
-		int inf = numeric_limits<int>::max();
-
-		
-
-
-
-
-		return;
-	}
-
-	// O(n^2)
-	void print(){
-		cout << endl;
-		vector<int> vertex = listAD->keys();
-		for (int i : vertex){
-			int vertice = vertex[i];
-			vector<vector<int>>* edges = listAD->get(vertice);
-			cout << "V: " << vertice << " A: ";
-			for(int j  = 0; j < edges->size();j++){
-				cout << "(" << vertice;
-				cout << "," << edges->at(j).at(0) << ") ";
-				cout << "[W: " << edges->at(j).at(1) << "] ";
-			}
+		// Printing tablaGrafos O(n^2)
+		void print(){
 			cout << endl;
+			cout << "#### IMPRIMIENDO GRAFOS ####" << endl;
+			vector<D> vertex = tablaGrafos.keys();
+			for (D i : vertex){
+				vector<pair<D, int>>* edges = tablaGrafos.get(i);
+				cout << "V: " << i << " A: ";
+				for(int j  = 0; j < edges->size(); j++){
+					// Print edge
+					cout << "(" << i;
+					cout << "," << edges->at(j).first << ") ";
+					// Print weight
+					cout << "[w:" << edges->at(j).second << "] ";
+				}
+				cout << endl;
+			}
 		}
-	}
 };
